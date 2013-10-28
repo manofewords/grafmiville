67047a2c77dd4d4254a3dd744ab33153dead54f1
Manuel Hitz <manuel.hitz@gmail.com>
2013-10-28 20:39:39 +0100

    add automatic horizontal scrolling (TODO: scrollTo(0,0) not working on page reload)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+// global variables... blerk! (TODO)
+var requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame,
+  doScroll = false,
+  scrollStart = 0,
+  scrollEnd = 0,
+  scrollIteration = 0;
+
+function animationLoop() {
+  var x = scrollStart + scrollIteration * 10;
+  if(doScroll) {
+    window.scrollTo(x, 0); // linear
+    scrollIteration++;
+    doScroll = (scrollEnd != x);
+  }
+  requestAnimationFrame(animationLoop);
+}
+animationLoop();
+
 function getBoxHandler(iteration, html) {
   return function(e) {
     var box = e.target;
@@ -6,6 +24,12 @@ function getBoxHandler(iteration, html) {
       case "expand" + iteration:
         box.className += " expanded";
         box.innerHTML = html;
+
+        // start a scroll animation
+        doScroll = true;
+        scrollStart = (iteration - 1) * 500;
+        scrollEnd = iteration * 500; // see $box1_x, $box2_x, $box3_x and $expandedWidth in main.scss (TODO: is there a way not to duplicate this from main.scss?)
+        scrollIteration = 0;
         break;
       case "shrink" + iteration:
         box.className = "box";
@@ -32,6 +56,6 @@ function getButtonHandler(iteration) {
     // expand next box
     style = document.getElementById("box" + (iteration + 1)).style;
     style.webkitAnimationName = "expand" + (iteration + 1);
-    style.webkitAnimationDelay = "3s"; // duration of Jump'n'Run animation (TODO: is there a way not to duplicate this from .scss file?)
+    style.webkitAnimationDelay = "3s"; // see $animDurationToRunAndJump in main.scss (TODO: is there a way not to duplicate this from main.scss?)
   };
 }
